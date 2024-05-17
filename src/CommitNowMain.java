445addf9f4342f2a6af9dfab8de6ce98ca12adaa
Casian Sebastian Chiriac <S.C.Chiriac@student.tudelft.nl>
2024-05-17 15:06:05 +0200

    c37

--- src/CommitNowMain.java
+++ src/CommitNowMain.java
@@ -14,7 +14,7 @@ public class CommitNowMain {
         checkProtocol() ;
         int commitStackSize = commitStackSizeCalculator() ;
         if(commitStackSize == 0){
-            System.out.println("Nothing to push, branch up to date"); /// asdfdsaf
+            System.out.println("Nothing to push, branch up to date"); /// asdfdsafdsaf
             return ;
         }
         String freshestCommit ;
