875e355ac34d6c4ce4903d914e7e28b6c4e1ce25
Casian Sebastian Chiriac <S.C.Chiriac@student.tudelft.nl>
2024-05-18 20:16:55 +0200

    c101

--- src/CommitNowMain.java
+++ src/CommitNowMain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-public class CommitNowMain {///123
+public class CommitNowMain {///12345124
     private static String remoteBranchName ;
     private static String localBranchName ;
     private static String pat_token ;
