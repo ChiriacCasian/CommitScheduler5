6265deeb38897496217c869898b2ba668161a9b0
Casian Sebastian Chiriac <S.C.Chiriac@student.tudelft.nl>
2024-06-14 11:58:56 +0200

    c5

--- src/CommitNowMain.java
+++ src/CommitNowMain.java
@@ -9,7 +9,7 @@ public class CommitNowMain {///123asdf
     private static String localBranchName ;
     private static String pat_token ;
     private static String ssh_key ;
-    public static void main(String[] args) throws IOException {//123123
+    public static void main(String[] args) throws IOException {//
         getProperties() ;
         checkProtocol() ;
 //        int commitStackSize = commitStackSizeCalculator() ;
